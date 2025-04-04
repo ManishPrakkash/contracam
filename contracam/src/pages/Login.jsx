a201e26accca9a6984296535541a486d43655d4a
ManishPrakkash <manishprakkash@gmail.com>
2025-04-04 13:57:57 +0000

    added changes in dashboard

--- contracam/src/pages/Login.jsx
+++ contracam/src/pages/Login.jsx
@@ -57,6 +57,7 @@ const Login = ({ setIsAuthenticated = () => {} }) => {
             <div className="bg-red-50 border-l-4 border-red-400 p-4 mb-6">
               <div className="flex">
                 <div className="ml-3">
+                
                   <p className="text-sm text-red-700">{error}</p>
                 </div>
               </div>
